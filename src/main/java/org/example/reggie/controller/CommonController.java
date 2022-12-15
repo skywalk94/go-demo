4a68233307d1d8027a216d93cedf090b0af3dafc
skywalk94 <skywalk94@foxmail.com>
2022-12-15 14:18:11 +0800

    上传文件

--- src/main/java/org/example/reggie/controller/CommonController.java
+++ src/main/java/org/example/reggie/controller/CommonController.java
@@ -1,21 +1,49 @@
 package org.example.reggie.controller;
 
 import org.example.reggie.common.R;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 @RestController
 @RequestMapping("/common")
 public class CommonController {
+    String path = "D:\\Desktop\\img\\";
+
     //        文件上传
     @PostMapping("/upload")
     public R<String> upload(MultipartFile file) throws IOException {
-        file.transferTo(new File("D:\\Desktop\\" + file.getOriginalFilename()));
-        return R.success("上传成功");
+        File dir = new File(path);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        String name = file.getOriginalFilename();
+        file.transferTo(new File(path + name));
+        return R.success(name);
+    }
+
+    //    文件下载
+    @GetMapping("/download")
+    public void download(String name, HttpServletResponse response) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream(new File(path + name));
+        ServletOutputStream outputStream = response.getOutputStream();
+        response.setContentType("image/jpeg");
+        int len = 0;
+        byte[] bytes = new byte[1024];
+        while ((len = fileInputStream.read(bytes)) != -1) {
+            outputStream.write(bytes, 0, len);
+            outputStream.flush();
+        }
+        outputStream.close();
+        fileInputStream.close();
     }
 }
